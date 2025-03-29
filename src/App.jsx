8e9ec159fed33c1210ca0a12d2f3c3c2dd7e7265
Gaurang More <gaurang.more@sunday.tech>
2025-03-29 20:19:00 +0530

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import './App.css'
+import "./App.css";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-600">
-      Hello world!
-    </h1>
+      <main className="bg-black">
+        <Navbar />
+        <Hero />
+        <Highlights />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
